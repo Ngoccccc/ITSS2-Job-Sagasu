5b78b26042d27de6eb05b7388f1fbd1d5dc8c410
khanhnguyenduy147 <86216942+khanhnguyenduy147@users.noreply.github.com>
2024-06-13 06:29:13 +0000

    Complete Frontend page employers allow to post

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import JobInfo from "./pages/JobInfo";
 import VerifyUser from "./pages/VerifyUser";
 import CreateSearchJobPost from "./pages/CreateSearchJobPost";
 import MyPost from "./pages/MyPost";
+import AllowPost from "./pages/employers/AllowPost";
 function App() {
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
         <Route path="/verify-user" element={<VerifyUser />} />
         <Route path="/create-post" element={<CreateSearchJobPost />} />
         <Route path="/my-post" element={<MyPost />} />
+        <Route path="/employers-post" element={<AllowPost />} />
       </Routes>
     </>
   );
---
+++ client/src/components/AllowPost.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "../styles/AllowPost.css";
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import axios from "axios";
+
+const UseAllowPost = () => {
+  const [useInfo, setUseInfo] = useState([]);
+
+  const postData = {
+    email: 'tranvana@example.com'
+};
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.post(`/api/user/get-active-user`, postData);
+      console.log('Response data:', response.data);
+      if (response.data) {
+        setUseInfo(response.data);
+      } else {
+        console.error('Unexpected response data format:', response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+    {useInfo.status === 'active' ? (
+        <div className="alert-popup">
+            <div>
+                <Alert  severity="success">Bạn có thể bắt đầu đăng bài.</Alert>
+                <div className="button-post">
+                    <Button  variant="outlined">Đăng bài</Button>
+                </div>
+            </div>
+        </div>
+    ) : (
+        <div className="alert-popup">
+            <Alert severity="warning">Tài khoản bạn chưa được xác minh.</Alert>
+        </div>
+    )}
+    </div>
+  );
+};
+
+export default UseAllowPost;
