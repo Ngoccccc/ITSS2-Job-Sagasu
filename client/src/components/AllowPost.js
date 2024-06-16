2475a75f6f825b31bc244c0d6ff08e6f1e766b94
Ngoccccc <trantienngoc2911@gmail.com>
2024-06-16 09:57:08 +0700

    check

--- client/src/components/AllowPost.js
+++ client/src/components/AllowPost.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "../styles/AllowPost.css";
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
 import axios from "axios";
 
 const UseAllowPost = () => {
   const [useInfo, setUseInfo] = useState([]);
 
   const postData = {
-    email: 'tranvana@example.com'
-};
+    email: "tranvana@example.com",
+  };
 
   const fetchData = async () => {
     try {
       const response = await axios.post(`/api/user/get-active-user`, postData);
-      console.log('Response data:', response.data);
+      console.log("Response data:", response.data);
       if (response.data) {
         setUseInfo(response.data);
       } else {
-        console.error('Unexpected response data format:', response.data);
+        console.error("Unexpected response data format:", response.data);
       }
     } catch (error) {
-      console.error('Error fetching posts:', error);
+      console.error("Error fetching posts:", error);
     }
   };
 
@@ -31,20 +31,17 @@ const UseAllowPost = () => {
 
   return (
     <div className="container">
-    {useInfo.status === 'active' ? (
+      {useInfo.status === "active" ? (
         <div className="alert-popup">
-            <div>
-                <Alert  severity="success">Bạn có thể bắt đầu đăng bài.</Alert>
-                <div className="button-post">
-                    <Button  variant="outlined">Đăng bài</Button>
-                </div>
-            </div>
+          <div>
+            <Alert severity="success">Bạn có thể bắt đầu đăng bài.</Alert>
+          </div>
         </div>
-    ) : (
+      ) : (
         <div className="alert-popup">
-            <Alert severity="warning">Tài khoản bạn chưa được xác minh.</Alert>
+          <Alert severity="warning">Tài khoản bạn chưa được xác minh.</Alert>
         </div>
-    )}
+      )}
     </div>
   );
 };
